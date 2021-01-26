51b1b209a017858b699d007855d384a3ba90061f
Samuel Tregea <sdtregea@gmail.com>
2021-01-26 07:22:54 -0500

    Updated directory name from 'phases' to 'drivers'

--- src/drivers/phase1.c
+++ src/drivers/phase1.c
@@ -2,12 +2,12 @@
  * CSCI-421 Project: Phase1
  * @file phase1.c
  * Description:
- *      This file serves at the client program for Phase 1 and will serve as the 'storage manager' which calls functions
+ *      This file serves as the main driver for Phase 1 and will serve as the 'storage manager' which calls functions
  *      implemented within '../c/storagemanager.c'.
  *
  * @authors Samuel Tregea, Kyle Collins, Geoffrey Moss, Sam Tillinghast
  */
-#include "../headers/storagemanager.h"
+#include "src/headers/storagemanager.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
