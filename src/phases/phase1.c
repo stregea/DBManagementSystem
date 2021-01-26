2341b74f9679898669d3b9be0a1088464d7214fb
Samuel Tregea <sdtregea@gmail.com>
2021-01-25 19:50:18 -0500

    Creation of the main file for Phase1.

---
+++ src/phases/phase1.c
@@ -0,0 +1,19 @@
+/**
+ * CSCI-421 Project: Phase1
+ * @file phase1.c
+ * Description:
+ *      This file serves at the client program for Phase 1 and will serve as the 'storage manager' which calls functions
+ *      implemented within '../c/storagemanager.c'.
+ *
+ * @authors Samuel Tregea, Kyle Collins, Geoffrey Moss, Sam Tillinghast
+ */
+#include "../headers/storagemanager.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc, char * argv[]){
+    printf("Hideous Jimmies!!");
+    return EXIT_SUCCESS;
+}
