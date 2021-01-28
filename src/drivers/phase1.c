68bf042dfc40f49b19d7bf2b8f4fa1710d289e5a
Samuel Tregea <sdtregea@gmail.com>
2021-01-28 18:25:57 -0500

    Updated CMakeList.txt to allow for CLion build

--- src/drivers/phase1.c
+++ src/drivers/phase1.c
@@ -5,7 +5,10 @@
  *      This file serves as the main driver for Phase 1 and will serve as the 'storage manager' which calls functions
  *      implemented within '../c/storagemanager.c'.
  *
- * @authors Samuel Tregea, Kyle Collins, Geoffrey Moss, Sam Tillinghast
+ * @author Kyle Collins  (kxc1981@rit.edu)
+ * @author Geoffrey Moss (gbm2613@rit.edu)
+ * @author Sam Tillinghast  (sft6463@rit.edu)
+ * @author Samuel Tregea  (sdt1093@rit.edu)
  */
 #include "../headers/storagemanager.h"
 #include <stdio.h>
@@ -14,6 +17,6 @@
 #include <stdbool.h>
 
 int main(int argc, char * argv[]){
-    printf("Hideous Jimmies!!");
+    printf("Hideous Jimmies!!\n");
     return EXIT_SUCCESS;
 }
