25e610ffc3d6af3e375533e9f2cbc0a46f4c2905
Geoffrey Moss <geoffmoss0@gmail.com>
2021-02-21 00:28:03 -0500

    Finished up insert_record function, surrounding parts may or may not be complete. Existing testing is enough to show moderate functionality, but is by no means comprehensive

--- src/drivers/phase1.c
+++ src/drivers/phase1.c
@@ -16,8 +16,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#define PAGE_SIZE 4096
-#define BUFFER_SIZE 1
+#define PAGE_SIZE 1536
+#define BUFFER_SIZE 10
 
 
 int main(int argc, char * argv[]){
@@ -36,25 +36,25 @@ int main(int argc, char * argv[]){
     int data_types_size = 3;
     int data_types[] = {3, 0, 1};
     int key_indices_size = 2;
-    int key_indices[] = {0, 1};
+    int key_indices[] = {3, 0};
     int table_id = add_table(data_types, key_indices, data_types_size, key_indices_size);
 
     // table(name, age, weight)
-    union record_item person_1[] = { {.c = "Bob"}, {.i = 31}, {.d = 168.23} };
-    union record_item person_6[] = { {.c = "Bob"}, {.i = 31}, {.d = 168.23} };
-    union record_item person_2[] = { {.c = "John"}, {.i = 23}, {.d = 180.20} };
-    union record_item person_3[] = { {.c = "George"}, {.i = 24}, {.d = 180.20} };
-    union record_item person_4[] = { {.c = "Paul"}, {.i = 26}, {.d = 180.20} };
-    union record_item person_5[] = { {.c = "Bob"}, {.i = 28}, {.d = 180.20} };
+    union record_item person_1[] = { {.c = "Bob\0"}, {.i = 31}, {.d = 168.23} };
+    union record_item person_2[] = { {.c = "John\0"}, {.i = 23}, {.d = 180.20} };
+    union record_item person_3[] = { {.c = "George\0"}, {.i = 24}, {.d = 180.20} };
+    union record_item person_4[] = { {.c = "Paul\0"}, {.i = 26}, {.d = 180.20} };
+    union record_item person_5[] = { {.c = "Bob\0"}, {.i = 28}, {.d = 180.20} };
+    union record_item person_6[] = { {.c = "Bob\0"}, {.i = 31}, {.d = 168.23} };
 
-    //insert_record(table_id, person_2);
+    insert_record(table_id, person_2);
     insert_record(table_id, person_1);
-    //insert_record(table_id, person_6);
+    insert_record(table_id, person_6);
     insert_record(table_id, person_3);
-    //insert_record(table_id, person_4);
+    insert_record(table_id, person_4);
     insert_record(table_id, person_5);
 
-    
+
 
 
    // terminate_database();
