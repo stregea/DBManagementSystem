05f4c73369c8bed9a98c3c13cf1e4b5b48605b95
Geoffrey Moss <geoffmoss0@gmail.com>
2021-02-20 19:58:59 -0500

    Rewrote some of insert_record, patched up some other things. Still need to add creating new pages

--- src/drivers/phase1.c
+++ src/drivers/phase1.c
@@ -16,7 +16,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#define PAGE_SIZE 10
+#define PAGE_SIZE 4096
 #define BUFFER_SIZE 1
 
 
@@ -35,14 +35,28 @@ int main(int argc, char * argv[]){
     // add table
     int data_types_size = 3;
     int data_types[] = {3, 0, 1};
-    int key_indices_size = 1;
-    int key_indices[] = {0};
+    int key_indices_size = 2;
+    int key_indices[] = {0, 1};
     int table_id = add_table(data_types, key_indices, data_types_size, key_indices_size);
 
     // table(name, age, weight)
     union record_item person_1[] = { {.c = "Bob"}, {.i = 31}, {.d = 168.23} };
+    union record_item person_6[] = { {.c = "Bob"}, {.i = 31}, {.d = 168.23} };
     union record_item person_2[] = { {.c = "John"}, {.i = 23}, {.d = 180.20} };
- 
+    union record_item person_3[] = { {.c = "George"}, {.i = 24}, {.d = 180.20} };
+    union record_item person_4[] = { {.c = "Paul"}, {.i = 26}, {.d = 180.20} };
+    union record_item person_5[] = { {.c = "Bob"}, {.i = 28}, {.d = 180.20} };
+
+    //insert_record(table_id, person_2);
+    insert_record(table_id, person_1);
+    //insert_record(table_id, person_6);
+    insert_record(table_id, person_3);
+    //insert_record(table_id, person_4);
+    insert_record(table_id, person_5);
+
+
+
+
   //  insert_record(table_id, person_1); -- mem leaks in here
    // insert_record(table_id, person_2);
 
