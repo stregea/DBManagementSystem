3b2a5ede8d5fc01bc82c8085863cc255779230db
chiefcollins <kscollins31@gmail.com>
2021-02-25 14:24:16 -0500

    read in statements from user input

---
+++ src/drivers/database.c
@@ -0,0 +1,121 @@
+#include "../headers/database.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_TOKEN_SIZE 20
+
+/*
+ * This function will be used to execute SQL statements that
+ * do not return any data. For instance, schema modification,
+ * insertion, delete, and update statements.
+ *
+ * @param statement - the SQL statement to execute.
+ * @return 0 if the statement is executed successfully
+           -1 otherwise
+ */
+int execute_non_query(char * statement){
+    return 0;
+}
+
+/*
+ * This function will be used to execute SQL statement that
+ * return data. For instance, SQL select queries.
+ *
+ * @param query - the SQL query to execute
+ * @param result - a 2d array of record_item (output variable).
+ *                This will be used to output the values of the query.
+                  This will be a pointer to the first item in the 2d array.
+                  The user of the function will be resposible for freeing.
+ * @return the number of tuples in the result, -1 if upon error.
+ */
+int execute_query(char * query, union record_item *** result){
+    return 0;
+}
+
+/*
+ * This function is resposible for safely shutdown the database.
+ * It will store to hardware any data needed to restart the database.
+ * @return 0 on success; -1 on failure.
+ */
+int shutdown_database(){
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    
+    if(argc < 3) {
+        fprintf(stderr, "%s", "usage: ./database <db_loc> <page_size> <buffer_size>\n");
+    }
+
+    char *databasePath = argv[1];
+    int pageSize = (int)strtol(argv[2], NULL, 10);
+    int bufferSize = (int)strtol(argv[3], NULL, 10);
+
+    //printf("db_loc: %s\npage_size: %d\nbuffer_size: %d\n", databasePath, pageSize, bufferSize);
+
+    FILE *databaseFile = fopen(databasePath, "r");
+    if(databaseFile) {
+        fclose(databaseFile);
+        //printf("restarting database ...\n");
+        //restart_database(databasePath);
+    } else {
+        //printf("restarting database ...\n");
+        //new_database(databasePath, pageSize, bufferSize);
+    }
+
+    // initialize with first token
+    char token[MAX_TOKEN_SIZE];
+    scanf("%s", token);
+    int tokenLength = strlen(token);
+    
+    int statementCount = 0;
+
+    while(strcmp(token, "quit;")) {
+        
+        // read in next token for next statement
+        if(statementCount > 0) {
+            scanf("%s", token);
+            //printf("%s", token);
+            tokenLength = strlen(token);
+        }
+
+        // initialize statement string
+        int statementLength = tokenLength;
+        char *statement = malloc(statementLength + 1);
+        strlcpy(statement, token, statementLength + 1);
+
+        // temp to hold statement contents when reading in statement
+        char *temp = malloc(statementLength + 1);
+        
+        // stop if the ';' character is within the token
+        while(token[tokenLength - 1] != ';') {
+
+            // read next token 
+            scanf("%s", token);
+            //printf("%s", token);
+            tokenLength = strlen(token);
+
+            // copy statement string to temp string 
+            temp = realloc(temp, statementLength + 1);
+            strlcpy(temp, statement, statementLength + 1);
+
+            // resize the statement memory to hold the contains in new token
+            // memblock = [Statement] + [sizeofchar] + [Token] + [NullTerminator]
+            statement = realloc(statement, statementLength + sizeof(char) + tokenLength + 1);
+            strlcpy(statement, temp, statementLength + 1);
+            statementLength = statementLength + sizeof(char) + tokenLength;
+            strlcat(statement, " ", statementLength + 1);
+            strlcat(statement, token, statementLength + 1);
+
+        }
+
+        // STOPPED HERE call the method to parse the statement
+        //printf("\nStatement:\n%s\n", statement);
+        free(statement);
+        free(temp);
+        statementCount++;
+
+    }
+
+}
