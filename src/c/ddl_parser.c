2f42a0b53a81080eed3469993294d3b093c6ba5a
Samuel Tregea <sdtregea@gmail.com>
2021-03-05 20:31:45 -0500

    Create new structs, add more parsing functionality to create commands

--- src/c/ddl_parser.c
+++ src/c/ddl_parser.c
@@ -5,6 +5,62 @@
 #include <stdlib.h>
 
 /**
+ * Struct to contain foreign key information.
+ */
+struct ForeignKey {
+    char *table_name; // name of referenced table
+    char *column_name; // name of referenced column
+};
+
+struct Constraints {
+    bool notnull;
+    bool primary_key;
+    bool unique;
+};
+
+/**
+ * Struct to contain information in regards to a attribute/column.
+ */
+struct Attribute {
+    char *name; // name of the attribute (column)
+    int type; // the type of data within the column (0-4) / int-varchar
+    int size; // used to determine the size of a char or varchar.
+    struct Constraints constraints;
+};
+
+/**
+ * Struct used to represent a Table.
+ *
+ * This is created within the create table command, or read from the catalog.
+ */
+struct Table {
+    int tableId;
+    char *name; // the name of the table
+    struct Attribute *attributes; // Array to hold the attributes/columns of a table.
+    int *key_indices; // array to contain the information to create a primary key.
+    int *data_types; // array to contain the data types found within a table.
+    union record_item **unique; // 2-D array to contain only the unique tuples that can be found in the table.
+    struct ForeignKey *foreignKey; // foreign key to reference another table.
+    int primary_key_count; // count used to kep track of the # of primary keys that exist within a table. 1 Max.
+    int foreign_key_count; // count used to kep track of the # of foreign keys that exist within a table.
+    int attribute_count; // count used to kep track of the # of atributes/columns that exist within a table.
+};
+// TODO: Catalog.
+
+void freeTable(struct Table table);
+
+int get_attribute_type(char *attribute);
+
+int add_primary_key_to_table(struct Table table, int *key_indices);
+
+/**
+ * Parse through the SQL statement the user entered.
+ * @param statement - the statement to parse.
+ * @return 0 upon success, -1 upon error.
+ */
+int parseStatement(char *statement);
+
+/**
  * Parse through the Create Table command.
  * @param tokenizer - the tokenizer to parse a command.
  * @param token - The token used for string tokenizing.
@@ -28,12 +84,7 @@ int parseDrop(char *tokenizer, char **token);
  */
 int parseAlter(char *tokenizer, char **token);
 
-/**
- * Parse through the SQL statement the user entered.
- * @param statement - the statement to parse.
- * @return 0 upon success, -1 upon error.
- */
-int parseStatement(char *statement);
+int parseAttributes(struct Table table, char *tokenizer);
 
 /**
  * This function handles the parsing of DDL statements
@@ -119,7 +170,7 @@ int parseStatement(char *statement) {
 int parseDrop(char *tokenizer, char **token) {
     tokenizer = strtok_r(NULL, " ", token);
     // table <name>
-    if (strcasecmp(tokenizer, "table") == 0) {
+    if (tokenizer != NULL && strcasecmp(tokenizer, "table") == 0) {
         printf("%s\n", tokenizer);
 
         // read in the table name
@@ -127,12 +178,12 @@ int parseDrop(char *tokenizer, char **token) {
 
         if (tokenizer != NULL && strcasecmp(tokenizer, "") != 0) {
             printf("%s\n", tokenizer);
-            // read table from disk
-            // get table id
-            // drop the table with table od
-            // drop_table(0);
             // TODO
-            char* table_name = malloc(strlen(tokenizer) + 1);
+            // read table from catalog
+            // get table id
+            // drop the table with table id
+            // drop_table(0);
+            char *table_name = malloc(strlen(tokenizer) + 1);
             strcpy(table_name, tokenizer);
             // read table from disk
 
@@ -156,7 +207,7 @@ int parseDrop(char *tokenizer, char **token) {
 int parseAlter(char *tokenizer, char **token) {
     tokenizer = strtok_r(NULL, " ", token);
 
-    if (strcasecmp(tokenizer, "table") == 0) {
+    if (tokenizer != NULL && strcasecmp(tokenizer, "table") == 0) {
         printf("%s\n", tokenizer);
 
         // read in the table name
@@ -178,7 +229,7 @@ int parseAlter(char *tokenizer, char **token) {
                     printf("%s\n", tokenizer);
 
                     // TODO
-                    char* table_name = malloc(sizeof(char*)*strlen(tokenizer)+1);
+                    char *table_name = malloc(sizeof(char *) * strlen(tokenizer) + 1);
                     strcpy(table_name, tokenizer);
                     // read table from disk
 
@@ -188,8 +239,8 @@ int parseAlter(char *tokenizer, char **token) {
                 return -1; // incorrect syntax/structure
             }
 
-            // add <a_name> <a_type>
-            // add <a_name> <a_type> default <value>
+                // add <a_name> <a_type>
+                // add <a_name> <a_type> default <value>
             else if (tokenizer != NULL && strcasecmp(tokenizer, "add") == 0) {
                 printf("%s\n", tokenizer);
                 // read <a_name>
@@ -222,7 +273,7 @@ int parseAlter(char *tokenizer, char **token) {
                             // read <value>
                             tokenizer = strtok_r(NULL, " ", token);
 
-                            if(tokenizer != NULL){
+                            if (tokenizer != NULL) {
                                 printf("%s\n", tokenizer);
                                 value = malloc(sizeof(char *) * strlen(tokenizer) + 1);
                                 strcpy(value, tokenizer);
@@ -275,7 +326,185 @@ int parseAlter(char *tokenizer, char **token) {
     return -1;
 }
 
-int parseCreate(char *command, char **token) {
+int parseCreate(char *tokenizer, char **token) {
     // TODO
-    return 0;
+
+    struct Table table_data; // add create_table function?
+    table_data.attribute_count = 0;
+    table_data.primary_key_count = 0;
+    table_data.foreign_key_count = 0;
+
+    tokenizer = strtok_r(NULL, " ", token);
+
+    if (tokenizer != NULL && strcasecmp(tokenizer, "table") == 0) {
+
+        // read in Table name
+        tokenizer = strtok_r(NULL, " (", token);
+        if (tokenizer != NULL) {
+            table_data.name = malloc(sizeof(char *) * strlen(tokenizer) + 1);
+            strcpy(table_data.name, tokenizer);
+
+            // perform loop to read in rest of attributes and populate Table struct
+            while (tokenizer != NULL) {
+
+                /**
+                 *
+                 * CREATE TABLE BAZZLE( baz double PRIMARYKEY ); // need to optimize parser for this later
+                 *
+                 *   create table foo( // working on parsing this
+                 *       baz integer,
+                 *       bar Double notnull,
+                 *       primarykey( bar baz ),
+                 *       foreignkey( bar ) references bazzle( baz )
+                 *   );
+                 *
+                 *
+                 */
+                // read in table information
+                tokenizer = strtok_r(NULL, "(,", token);
+
+                // this can potentially be null -> prevent any potential segfaults for bad reads
+                if (tokenizer != NULL) {
+
+                    // check for any keywords
+                    if (strcasecmp(tokenizer, "primarykey") == 0) {
+                        // set and create table primary key
+                        // may want to create a function here
+                        tokenizer = strtok_r(NULL, "(,", token);
+                    } else if (strcasecmp(tokenizer, "foreignkey") == 0) {
+                        // set delim as ), in tokenizer
+                        // set and create foreign key
+                        tokenizer = strtok_r(NULL, "(,", token);
+                        // parse references table_name(table_col)
+                        // may want to create a function here
+                    } else if (strcasecmp(tokenizer, "unique") == 0) {
+                        // set delim as ), in tokenizer
+                        // set unique attributes
+                        // may want to create a function here
+                    } else { // parse through the attributes/column information
+                        int result = parseAttributes(table_data, tokenizer);
+                        if (result == -1) {
+                            freeTable(table_data);
+                            return -1;
+                        }
+                    }
+                }
+            }
+            // write table to catalog
+
+//            freeTable(table_data);
+            return 0;
+        }
+    }
+    fprintf(stderr, "Error: Invalid command.\n");
+    return -1;
+}
+
+// TODO
+int parseAttributes(struct Table table, char *tokenizer) {
+    printf("%s\n", tokenizer);
+
+    char* temp_token;
+    tokenizer = strtok_r(tokenizer, " ", &temp_token); // split the string via spaces
+    struct Attribute attribute;
+
+    // read in attribute/ column name
+    if (tokenizer != NULL) {
+        attribute.name = malloc(sizeof(char *) * strlen(tokenizer) + 1);
+        attribute.constraints.primary_key = false;
+        attribute.constraints.notnull = false;
+        attribute.constraints.unique = false;
+
+        strcpy(attribute.name, tokenizer);
+
+        // read in attribute type
+        tokenizer = strtok_r(NULL, " ", &temp_token);
+        if (tokenizer != NULL) {
+
+            // read in column type, function returns 0-4 based on string name (integer-varchar)
+            attribute.type = get_attribute_type(tokenizer);
+
+            // if type == -1 -> error
+            // if type == char || varchar
+            // TODO
+            if (attribute.type == 3 || attribute.type == 4) {
+                // set size of attribute
+            }
+
+            // loop through constraints
+            while (tokenizer != NULL) {
+                tokenizer = strtok_r(NULL, " )", &temp_token);
+
+                if (tokenizer != NULL) {
+                    // TODO
+                    if (strcasecmp(tokenizer, "primarykey") == 0) {
+                        attribute.constraints.primary_key = true;
+                        // TODO: set primary key for table
+                    } else if (strcasecmp(tokenizer, "unique") == 0) {
+                        // TODO: add unique contraint to table
+                        attribute.constraints.unique = true;
+                    } else if (strcasecmp(tokenizer, "notnull") == 0) {
+                        // flag attribute as notnull
+                        attribute.constraints.notnull = true;
+                    }
+                    // else error?
+                }
+            }
+        }
+        table.attribute_count++;
+        table.attributes = realloc(table.attributes,
+                                   sizeof(struct Attribute)*table.attribute_count); // this is currently not being set anywhere. need to set default to 0
+        table.attributes[table.attribute_count-1] = attribute;
+        // read as column/attribute // set delim as ), in tokenizer
+        // split string through space
+        // read in column name
+        return 0;
+    }
+    return -1;
+}
+
+void freeTable(struct Table table) {
+    free(table.name);
+    free(table.key_indices);
+    free(table.data_types);
+    // TODO: free unique array -- not to sure how I want to save this yet.
+
+    // free foreign keys
+    for (int i = 0; i < table.foreign_key_count; i++) {
+        free(table.foreignKey[i].table_name);
+        free(table.foreignKey[i].column_name);
+    }
+
+    // free attributes
+    for (int i = 0; i < table.attribute_count; i++) {
+        free(table.attributes[i].name);
+//        free(table.attributes[i].constraints);
+    }
+}
+
+int add_primary_key_to_table(struct Table table, int *key_indices) {
+
+    if (table.primary_key_count == 0) {
+        table.key_indices = key_indices;
+    }
+    return -1; // error since primary key already exists in table.
+}
+
+int get_attribute_type(char *type) {
+
+    if (strcasecmp(type, "integer") == 0) {
+        return 0;
+    } else if (strcasecmp(type, "double") == 0) {
+        return 1;
+
+    } else if (strcasecmp(type, "bool") == 0) {
+        return 2;
+
+    } else if (strcasecmp(type, "bool") == 0) {
+        return 3;
+
+    } else if (strcasecmp(type, "bool") == 0) {
+        return 4;
+    }
+    return -1;
 }
--- src/drivers/database.c
+++ src/drivers/database.c
@@ -67,71 +67,7 @@ int main(int argc, char *argv[]) {
         new_database(databasePath, pageSize, bufferSize);
     }
 
-    // initialize with first token
-    char token[MAX_TOKEN_SIZE];
-    scanf("%s", token);
-    int tokenLength = strlen(token);
 
-    int statementCount = 0;
-
-    while(strcmp(token, "quit;")) {
-
-        // read in next token for next statement
-        if(statementCount > 0) {
-            scanf("%s", token);
-            //printf("%s", token);
-            tokenLength = strlen(token);
-        }
-
-        // initialize statement string
-        int statementLength = tokenLength;
-        char *statement = malloc(statementLength + 1);
-        strncpy(statement, token, statementLength + 1);
-
-        // temp to hold statement contents when reading in statement
-        char *temp = malloc(statementLength + 1);
-
-        // stop if the ';' character is within the token
-        while(token[tokenLength - 1] != ';') {
-
-            // read next token
-            scanf("%s", token);
-            //printf("%s", token);
-            tokenLength = strlen(token);
-
-            // copy statement string to temp string
-            temp = realloc(temp, statementLength + 1);
-            strncpy(temp, statement, statementLength + 1);
-
-            // resize the statement memory to hold the contains in new token
-            // memblock = [Statement] + [sizeofchar] + [Token] + [NullTerminator]
-            statement = realloc(statement, statementLength + sizeof(char) + tokenLength + 1);
-            strncpy(statement, temp, statementLength + 1);
-            statementLength = statementLength + sizeof(char) + tokenLength;
-            strncat(statement, " ", statementLength + 1);
-            strncat(statement, token, statementLength + 1);
-
-        }
-
-        //printf("\nStatement:\n%s\n", statement);
-
-        parse_ddl_statement(statement);
-
-        free(statement);
-        free(temp);
-        statementCount++;
-    }
-
-    /**
-    // add table
-    int data_types_size = 3;
-    int data_types[] = {3, 0, 1};
-    int key_indices_size = 2;
-    int key_indices[] = {3, 0};
-    int table_id = add_table(data_types, key_indices, data_types_size, key_indices_size);
-
-    /// testing different commands.
-    // 'proper' statements
     parse_ddl_statement("DROP TABLE HELLO;THIS IS A TEST;drop table sam;");
     parse_ddl_statement("alter table foo drop bar;");
     parse_ddl_statement("alter table foo add gar double;");
@@ -146,6 +82,101 @@ int main(int argc, char *argv[]) {
     parse_ddl_statement("alter table foo add gar");
     parse_ddl_statement("alter table foo add far double default");
     parse_ddl_statement("alter table foo add far double blah"); // should we error if 'default' not read?
+
+    // testing create statements.
+    parse_ddl_statement("CREATE TABLE BAZZLE( baz double PRIMARYKEY );");
+    parse_ddl_statement("CREATE TABLE BAZZLE( baz integer,"
+                        "bar Double notnull primarykey,"
+                        "bar Double notnull primarykey,"
+                        "bar Double notnull primarykey,");
+//                        "primarykey( bar baz ),"
+//                        "foreignkey( bar ) references bazzle( baz ) );");
+
+
+    // initialize with first token
+//    char token[MAX_TOKEN_SIZE];
+//    scanf("%s", token);
+//    int tokenLength = strlen(token);
+//
+//    int statementCount = 0;
+//
+//    while(strcmp(token, "quit;") != 0) {
+//
+//        // read in next token for next statement
+//        if(statementCount > 0) {
+//            scanf("%s", token);
+//            //printf("%s", token);
+//            tokenLength = (int)strlen(token);
+//        }
+//
+//        // initialize statement string
+//        int statementLength = tokenLength;
+//        char *statement = malloc(statementLength + 1);
+//        strncpy(statement, token, statementLength + 1);
+//
+//        // temp to hold statement contents when reading in statement
+//        char *temp = malloc(statementLength + 1);
+//
+//        // stop if the ';' character is within the token
+//        while(token[tokenLength - 1] != ';') { // builds the string from all the tokens.
+//
+//            // read next token
+//            scanf("%s", token);
+//            //printf("%s", token);
+//            tokenLength = strlen(token);
+//
+//            // copy statement string to temp string
+//            temp = realloc(temp, statementLength + 1);
+//            strncpy(temp, statement, statementLength + 1);
+//
+//            // resize the statement memory to hold the contains in new token
+//            // memblock = [Statement] + [sizeofchar] + [Token] + [NullTerminator]
+//            statement = realloc(statement, statementLength + sizeof(char) + tokenLength + 1);
+//            strncpy(statement, temp, statementLength + 1);
+//            statementLength = statementLength + sizeof(char) + tokenLength;
+//            strncat(statement, " ", statementLength + 1);
+//            strncat(statement, token, statementLength + 1);
+//
+//        }
+//
+//        //printf("\nStatement:\n%s\n", statement);
+//
+//        int result = parse_ddl_statement(statement);
+//
+//        if(result == -1){
+//            fprintf(stderr, "ERROR\n");
+//        }else{
+//            printf("SUCCESS");
+//        }
+//        free(statement);
+//        free(temp);
+//        statementCount++;
+//    }
+
+    /**
+    // add table
+    int data_types_size = 3;
+    int data_types[] = {3, 0, 1};
+    int key_indices_size = 2;
+    int key_indices[] = {3, 0};
+    int table_id = add_table(data_types, key_indices, data_types_size, key_indices_size);
+
+    /// testing different commands.
+    // 'proper' statements
+//    parse_ddl_statement("DROP TABLE HELLO;THIS IS A TEST;drop table sam;");
+//    parse_ddl_statement("alter table foo drop bar;");
+//    parse_ddl_statement("alter table foo add gar double;");
+//    parse_ddl_statement("alter table foo add far double default 10.1");
+//
+//    // bad statements
+//    parse_ddl_statement("DROP TABLE");
+//    parse_ddl_statement("alter table");
+//    parse_ddl_statement("alter table foo");
+//    parse_ddl_statement("alter table foo drop");
+//    parse_ddl_statement("alter table foo add");
+//    parse_ddl_statement("alter table foo add gar");
+//    parse_ddl_statement("alter table foo add far double default");
+//    parse_ddl_statement("alter table foo add far double blah"); // should we error if 'default' not read?
 
     //    terminate_database();
 
