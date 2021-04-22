18af5316cb16c9a3ac0c72fb8d54ebeadbdbb86b
Samuel Tregea <sdtregea@gmail.com>
2021-04-22 10:58:48 -0400

    Fixed most errors minus 4 memory leaks.

--- src/drivers/test_expressions.c
+++ src/drivers/test_expressions.c
@@ -22,7 +22,7 @@ void treeprint(Node root, int level) {
 }
 
 int main() {
-    char *expression = "21*34>=5*2*2";
+    char *expression = "21254*34>=5*2*2";
 
 
     OperationTree tree = build_tree(expression);
